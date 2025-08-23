b8a089da47ed980763b86dc0f6044e8cdf4c637e
sushantsinghss <111539378+sushantsinghss@users.noreply.github.com>
2025-08-23 00:24:26 +0000

    Deploying to gh-pages from @ sushantsinghss/sushantsinghss.github.io@d395fe3ce6e8dcebb791342e051e0ef3ca4e9677 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-publications",
           title: "publications",
-          description: "publications by categories in reversed chronological order.",
+          description: "publications by categories in reversed chronological order",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
