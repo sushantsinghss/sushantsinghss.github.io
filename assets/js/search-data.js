1ae9897f9e6dea8bd2904523e602a68892c2bf1c
sushantsinghss <111539378+sushantsinghss@users.noreply.github.com>
2025-08-23 17:00:46 +0000

    Deploying to gh-pages from @ sushantsinghss/sushantsinghss.github.io@75b2d0855c62d6d53416f1fc2c681ac774134568 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -432,6 +432,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-math-3310",
+          title: 'MATH 3310',
+          description: "Basic Real Analysis",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/F21math3310/";
             },},{
         id: 'social-email',
         title: 'email',
